d6e0b75a8235a6e710c49cd8094c34c9f7cf6ebe
Alisson Vieira <alisson.echo@gmail.com>
2023-11-16 08:10:23 -0300

    Ajustando configurações do vitest para os testes unitários

--- src/tests/views/Quina.test.js
+++ src/tests/views/Quina.test.js
@@ -29,53 +29,24 @@ describe('Quina.vue', () => {
       }
     });
 
-    // Verifica se o Navbar é renderizado
+    
     expect(wrapper.findComponent(Navbar).exists()).toBe(true);
-
-    // Verifica se o título principal está correto
+    
     expect(wrapper.find('h1.text-center').text()).toBe('Jogo da Quina');
-
-    // Verifica se a descrição está correta
+    
     expect(wrapper.find('p.text-center').text()).toBe('Selecione no mínimo 5 e no máximo 15 números para poder efetivar o jogo.');
-
-    // Verifica se o NumberSelector está renderizado
+    
     expect(wrapper.findComponent(NumberSelector).exists()).toBe(true);
-
-    // Verifica se o SelectedNumbersDisplay está renderizado
+    
     expect(wrapper.findComponent(SelectedNumbersDisplay).exists()).toBe(true);
-
-    // Verifica se o NumberCountDisplay está renderizado
+    
     expect(wrapper.findComponent(NumberCountDisplay).exists()).toBe(true);
-
-    // Verifica se o PriceDisplay está renderizado
+    
     expect(wrapper.findComponent(PriceDisplay).exists()).toBe(true);
-
-    // Verifica se o Button está renderizado
+    
     expect(wrapper.findComponent(Button).exists()).toBe(true);
     
   });
 
-  it('deve retornar falso quando a quantidade de números jogados for menor que 5', () => {
-    const wrapper = mount(Quina, {
-      global: {
-        components: {
-          Navbar,
-          NumberSelector,
-          SelectedNumbersDisplay,
-          NumberCountDisplay,
-          PriceDisplay,
-          Button
-        },
-        provide: {
-          router: {
-            push: vi.fn()
-          }
-        }
-      }
-    });
-    //given - dado que tenho
-    
-    //when - quando fizer
-    //then - então espero que
-  })
+  
 });
